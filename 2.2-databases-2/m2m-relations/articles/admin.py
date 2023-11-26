570c703c80528f7ef752ee0ecdd572fc7fecc4bf
Dima <dvnoskov88@gmail.com>
2023-11-27 00:56:02 +0300

    done

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,11 +1,36 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+
+from .models import Article, Scope
+
+
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        flag = 0
+        for form in self.forms:
+            # В form.cleaned_data будет словарь с данными
+            # каждой отдельной формы, которые вы можете проверить
+            if form.cleaned_data["is_main"] == True:
+                flag += 1
+            #print(form.cleaned_data)
+            # вызовом исключения ValidationError можно указать админке о наличие ошибки
+            # таким образом объект не будет сохранен,
+            # а пользователю выведется соответствующее сообщение об ошибке
+            if flag > 1 :
+                raise ValidationError('Только один тег может быть основным.')
+        return super().clean()  # вызываем базовый код переопределяемого метода
+
+
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
-
-
-
+    inlines = [ScopeInline]
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, verbose_name= 'Название')
-
+    
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
 
